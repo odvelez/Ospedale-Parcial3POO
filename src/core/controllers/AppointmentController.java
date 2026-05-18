1aceb97920e6ac0bbb1618d35e5dc67dbf65038e
lvillarreale <lvillarreale@uninorte.edu.co>
2026-05-18 13:48:58 -0500

    feat: add AppoimentController skeleton

---
+++ src/core/controllers/AppointmentController.java
@@ -0,0 +1,11 @@
+package core.controllers;
+
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+
+public class AppointmentController {
+
+    public static Response listPatientAppointments(long patientId) {
+        return new Response("Not implemented yet", Status.NOT_IMPLEMENTED);
+    }
+}
